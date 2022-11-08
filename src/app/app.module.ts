6259f42df799817e76e735a06b4983926e8d74ce
samuelvictorol <samuel.softdev@outlook.com>
2022-11-08 14:29:50 -0300

    Adiciona Navbar responsiva e rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { HomeComponent } from './home/home.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'sobre', component: SobreComponent},
+  {path: 'projetos', component: ProjetosComponent}
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SobreComponent,
+    HomeComponent,
+    ProjetosComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
