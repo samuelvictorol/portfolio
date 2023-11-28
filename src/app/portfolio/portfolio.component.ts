0c9db0e9d392aa6340039e31d806c0a191fab2b7
Samuel Victor Oliveira Lima <990905>
2023-11-28 14:27:30 -0300

    fixes portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -17,7 +17,7 @@ export class PortfolioComponent {
   }
 
    projects = [
-     { span: true,title: 'Weather App', desc: 'Aplicativo que mostra o clima ao redor do planeta e mostra uma imagem do lugar no background. Utilizando Vanilla JS e APIs externas do Unsplash e do WeatherApi.', url: 'https://samuelvictorol.github.io/WeatherApp/', img: 'assets/weather-app.png'},
+     { span: true,title: 'UnB na Mão', desc: 'WebApp Mobile First para facilitar a vida do estudante universitário da UnB, acelerando o acesso aos tópicos mais utilizados pelos alunos. Além de ter um sistema de cadastro de vagas e diferentes tipos de perfis com funcionalidades de ações diferentes.', url: 'https://unbnamao.netlify.app/', img: 'assets/unbnamao.png'},
      { img:'assets/solarsystem.png', title: 'Sistema Solar 3D', desc: 'Um projeto pessoal que consiste em um site que viaja pelo sistema solar em 3D utilizando a biblioteca Three.js', url:'https://github.com/samuelvictorol/Solar-System-3js'  },
      { title: 'Integração Paypal Angular', desc: 'Aplicação Web com integração com API do PayPal, Cart dinâmico utilizando diversos conceitos de Angular Framework e estilizado usando Bootstrap.', url: 'https://github.io/samuelvictorol/PayPal-Integration', img: 'assets/paypal.png', },
      { title: 'CAIXA - SIEVJ', img:'assets/sievjCaixaImg.png', url:"https://www.figma.com/proto/3NIN87Pxi4igP8BHzknUdp/SIEVJ?page-id=0%3A1&node-id=2%3A11&viewport=303%2C325%2C0.07&scaling=min-zoom&starting-point-node-id=2%3A11", desc: 'Projeto interno da CAIXA, um sistema de leilão de jóias onde atuei no desenvolvimento do frontend em Angular e na prototipagem de alta fidelidade com FIGMA. Código não disponível para visualização por se tratar de um projeto privado da empresa.', },
@@ -30,7 +30,7 @@ export class PortfolioComponent {
   }
 
   cardUrl(cardUrl: any) {
-    if(cardUrl.includes('.com') || cardUrl.includes('.io')){
+    if(cardUrl.includes('.com') || cardUrl.includes('.io') || cardUrl.includes('.app')){
       window.open(cardUrl, '_blank');
     } else if(cardUrl.includes('PUMA-Doc')){
       window.open(cardUrl, '_blank');
